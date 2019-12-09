250c947162b28f522ae21d91c5060f687c1764d3
Nihal Ismail <nihalismailk@gmail.com>
2019-12-09 23:46:22 +0530

    Product banner complete

--- lib/widgets/product_banner.dart
+++ lib/widgets/product_banner.dart
@@ -30,7 +30,54 @@ class ProductBanner extends StatelessWidget {
                   Color(0xFFFEB692),
                   Color(0xFFEA5455)
                 ]
-              )
+              ),
+              boxShadow: [
+                BoxShadow(
+                color: Colors.black12,
+                offset: Offset(0,4),
+                blurRadius: 4
+              ),
+                BoxShadow(
+                  color: Colors.black12,
+                  offset: Offset(0,-6),
+                  blurRadius: 4
+                )
+              ],
+              borderRadius: BorderRadius.circular(16),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  shoes.name,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold
+                  ),
+                ),
+                SizedBox(
+                  height: ScreenUtil().setHeight(22),
+                ),
+                Text(
+                  "\$${shoes.price}",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 24
+                  ),
+                )
+              ],
+            ),
+          ),
+          Positioned(
+            top: ScreenUtil().setHeight(-85),
+            right: ScreenUtil().setWidth(30),
+            child: Image.asset(
+              shoes.imageURL,
+              width: ScreenUtil().setWidth(610),
+              height: ScreenUtil().setHeight(550),
             ),
           )
         ],
