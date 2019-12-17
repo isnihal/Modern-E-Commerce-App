682a70e89dc2d9fd2b591eb6ebd6dfbec764ec42
Nihal Ismail <nihalismailk@gmail.com>
2019-12-17 18:48:27 +0530

    More progress

--- lib/screens/product_page.dart
+++ lib/screens/product_page.dart
@@ -7,20 +7,19 @@ class ProductPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var mediaQuery = MediaQuery.of(context);
+
     return Scaffold(
       body: Container(
         height: double.infinity,
         width: double.infinity,
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             Stack(
               children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: IconButton(icon: Icon(Icons.arrow_back),
-                    onPressed: () => Navigator.pop(context),),
-                ),
+
                 Stack(
                   children: <Widget>[
                     Align(
@@ -54,14 +53,68 @@ class ProductPage extends StatelessWidget {
                         )
                     ),
                     Container(
-                      height: 610,
-                      width: 640,
-                      child: Image.asset("assets/images/nike1.png")
+                      height: 510,
+                      width: 540,
+                      child: Image.asset("assets/images/nike1.png",)
                     )
                   ],
                 ),
-
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: IconButton(icon: Icon(Icons.arrow_back), onPressed: () => Navigator.pop(context),),
+                ),
               ],
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                width: double.infinity,
+                height: 75,
+                color: Theme.of(context).scaffoldBackgroundColor,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Container(
+                      width: mediaQuery.size.width/2.25,
+                      height: 75,
+                      child:  Center(
+                        child: Text(
+                            "\$375",
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 42
+                            )
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(
+                        height: 75,
+                        width: 100,
+                        child: Center(
+                          child: Text(
+                              "ADD TO CART",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18
+                              )
+                          ),
+                        ),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.only(topLeft: Radius.circular(60)),
+                            gradient: LinearGradient(
+                              colors: [Colors.red,Colors.red[200]],
+                              begin: Alignment.topRight,
+                              end: Alignment.bottomLeft
+                            ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
             )
           ],
         ),
