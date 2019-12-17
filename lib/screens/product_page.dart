bf7b963330711cf438e3d907dd6526ebf6fd02ef
Nihal Ismail <nihalismailk@gmail.com>
2019-12-17 18:19:19 +0530

    Triangle added

--- lib/screens/product_page.dart
+++ lib/screens/product_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shape_of_view/shape_of_view.dart';
 
 class ProductPage extends StatelessWidget {
 
@@ -7,10 +8,37 @@ class ProductPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: <Widget>[
-          IconButton(icon: Icon(Icons.arrow_back),onPressed: () => Navigator.pop(context),)
-        ],
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Stack(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: IconButton(icon: Icon(Icons.arrow_back),onPressed: () => Navigator.pop(context),),
+                ),
+                Align(
+                  alignment: Alignment.center,
+                  child: ShapeOfView(
+                    child: Container(
+                      height: 300,
+                      width: 300,
+                      color: Colors.redAccent,
+                    ),
+                    shape: TriangleShape(
+                        percentBottom: 0.5,
+                        percentLeft: 0,
+                        percentRight: 0
+                    ),
+                  )
+                  ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
