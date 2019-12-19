562b9c803096768f8f830ee152fba8d7d2b58b07
Nihal Ismail <nihalismailk@gmail.com>
2019-12-19 12:40:06 +0530

    Profile page complete

--- lib/screens/contact_screen.dart
+++ lib/screens/contact_screen.dart
@@ -1,5 +1,6 @@
 import 'package:assorted_layout_widgets/assorted_layout_widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:shape_of_view/shape_of_view.dart';
 
 class ContactScreen extends StatelessWidget {
@@ -29,27 +30,56 @@ class ContactScreen extends StatelessWidget {
                             "assets/images/mac.jpg", fit: BoxFit.fill,
                           )
                       ),
-                      Align(
-                        alignment: Alignment.bottomLeft,
-                        child: Container(
-                          margin: EdgeInsets.symmetric(horizontal: 24),
-                          height: 150,
-                          width: 150,
-                          child: ShapeOfView(
-                            shape: CircleShape(
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Container(
+                            margin: EdgeInsets.symmetric(horizontal: 24),
+                            height: 150,
+                            width: 150,
+                            child: ShapeOfView(
+                              shape: CircleShape(
+                              ),
+                              child: Image.asset("assets/images/nihal.jpg",),
                             ),
-                            child: Image.asset("assets/images/nihal.jpg",),
                           ),
-                        ),
+                          Expanded(
+                            child: Padding(
+                              padding: const EdgeInsets.only(right: 16,top: 16),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: <Widget>[
+                                  Padding(
+                                    padding: EdgeInsets.all(0),
+                                    child: Text("Nihal Ismail",style: TextStyle(
+                                        color: Colors.black,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 28
+                                    ),),
+                                  ),
+                                  Padding(
+                                    padding: EdgeInsets.all(0),
+                                    child: Text("Freelance Developer",style: TextStyle(
+                                        color: Colors.grey,
+                                        fontSize: 14
+                                    ),),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          )
+
+                        ],
                       ),
                     ],
                   ),
                   Expanded(
                     child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget>[
                         Padding(
-                          padding: const EdgeInsets.symmetric(horizontal:8.0,vertical: 16),
+                          padding: const EdgeInsets.symmetric(horizontal:10.0,vertical: 16),
                           child: Text(
                             "Imagining your Business like this?",
                               style: TextStyle(
@@ -58,6 +88,21 @@ class ContactScreen extends StatelessWidget {
                                   fontSize: 28
                               )
                           ),
+                        ),
+                        ListTile(
+                          onTap: (){},
+                          leading: Icon(FontAwesomeIcons.at,color: Colors.black87,),
+                          title: Text("nihal@nihalismail.com"),
+                        ),
+                        ListTile(
+                          onTap: (){},
+                          leading: Icon(FontAwesomeIcons.whatsapp,color: Colors.green,),
+                          title: Text("+91 7907136126"),
+                        ),
+                        ListTile(
+                          onTap: (){},
+                          leading: Icon(FontAwesomeIcons.globeAmericas,color: Colors.blue,),
+                          title: Text("www.nihalismail.com",style: TextStyle(color: Colors.black),),
                         )
                       ],
                     )
